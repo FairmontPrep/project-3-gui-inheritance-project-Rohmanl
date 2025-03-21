34bfd1a5e8df764594f4527bd670462bb1d42db0
Rohmanl <youkai8088@gmail.com>
2025-03-21 08:34:48 -0700

    finish project gui

--- clown.class/clown.java
+++ clown.class/clown.java
@@ -10,8 +10,8 @@ public class clown extends JFrame {
     public clown() {
         setTitle("Clown");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(850, 1500); 
-        setLocationRelativeTo(null); 
+        setSize(850, 1500);
+        setLocationRelativeTo(null);
 
         FacePanel facePanel = new FacePanel();
         add(facePanel);
@@ -19,32 +19,34 @@ public class clown extends JFrame {
         Timer timer = new Timer(2000, e -> facePanel.repaint());
         timer.start();
 
-
         setVisible(true);
     }
 
     class FacePanel extends JPanel {
         private BufferedImage head, eyes, face;
         private BufferedImage[] mouths;
-        private Random random;
-
+        private int selectedMouthIndex;
+        private String[] mouthColors = {"blue", "red", "green", "default"};
+        private String selectedMouthColor;
 
         public FacePanel() {
-            random = new Random();
+            Random random = new Random();
             mouths = new BufferedImage[4];
 
-            
             try {
                 head = ImageIO.read(new File("head.png"));
                 eyes = ImageIO.read(new File("eyes.png"));
                 face = ImageIO.read(new File("face.png"));
-                mouths[0] = ImageIO.read(new File("blueMouth.png"));  
-                mouths[1] = ImageIO.read(new File("redMouth.png"));   
+                mouths[0] = ImageIO.read(new File("blueMouth.png"));
+                mouths[1] = ImageIO.read(new File("redMouth.png"));
                 mouths[2] = ImageIO.read(new File("greenMouth.png"));
                 mouths[3] = ImageIO.read(new File("mouth.png"));
             } catch (IOException e) {
                 System.out.println("Error loading images: " + e.getMessage());
             }
+
+            selectedMouthIndex = random.nextInt(mouths.length);
+            selectedMouthColor = mouthColors[selectedMouthIndex];
         }
 
         @Override
@@ -56,12 +58,19 @@ public class clown extends JFrame {
             if (face != null) g2d.drawImage(face, 0, -80, this);
             if (eyes != null) g2d.drawImage(eyes, 0, -80, this);
 
-            if (mouths != null && mouths.length > 0) {
-                int randomIndex = random.nextInt(mouths.length);
-                if (mouths[randomIndex] != null) {
-                    g2d.drawImage(mouths[randomIndex], 0, -80, this);
-                }
+            if (mouths[selectedMouthIndex] != null) {
+                g2d.drawImage(mouths[selectedMouthIndex], 0, -80, this);
             }
+
+            g2d.setFont(new Font("Arial", Font.PLAIN, 14));
+            g2d.setColor(Color.BLACK);
+            int y = 50;
+            Text text = new Text("This", 50, y);
+            text = new ClownText(text, 50, y += 20);
+            text = new InheritedText(text, 50, y += 20);
+            text = new ColorText(text, selectedMouthColor, 50, y += 20);
+            text = new MouthText(text, 50, y += 20);
+            text.draw(g2d);
         }
 
         @Override
@@ -69,11 +78,50 @@ public class clown extends JFrame {
             if (head != null) {
                 return new Dimension(head.getWidth(), head.getHeight());
             }
-            return new Dimension(400, 400); 
+            return new Dimension(400, 400);
+        }
+    }
+
+    class Text {
+        protected String text;
+        protected int x, y;
+
+        public Text(String text, int x, int y) {
+            this.text = text;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void draw(Graphics2D g2d) {
+            g2d.drawString(text, x, y);
+        }
+    }
+
+    class ClownText extends Text {
+        public ClownText(Text previous, int x, int y) {
+            super(previous.text + " clown", x, y);
+        }
+    }
+
+    class InheritedText extends Text {
+        public InheritedText(Text previous, int x, int y) {
+            super(previous.text + " has inherited", x, y);
+        }
+    }
+
+    class ColorText extends Text {
+        public ColorText(Text previous, String color, int x, int y) {
+            super(previous.text + " a " + color, x, y);
+        }
+    }
+
+    class MouthText extends Text {
+        public MouthText(Text previous, int x, int y) {
+            super(previous.text + " mouth", x, y);
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new clown());
+        SwingUtilities.invokeLater(clown::new);
     }
 }
